9c4fbda006e17120bebba8a8e45a1e4100dbeaa8
josemanuel <toroj6231@gmail.com>
2026-05-07 18:56:01 -0400

    iniciando prueba tecnica

---
+++ src/test/java/com/Josecode/SistemdeGestiondeReservasconConcurrencia/SistemdeGestiondeReservasconConcurrenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Josecode.SistemdeGestiondeReservasconConcurrencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemdeGestiondeReservasconConcurrenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
